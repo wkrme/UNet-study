ae0b2c09fa30f58bb3d4deec01356760e552cbda
wkrme <wkrme234@gmail.com>
2025-10-28 00:17:34 +0900

    add-scheduler-and-optimizer

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ from torch.utils.data import random_split, DataLoader, Subset
 from model import UNet
 from dataset import UNetDataset
 from loss import BCELoss
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 # Config #
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -28,7 +29,7 @@ val_size = len(dataset) - train_size
 
 train_dataset, valid_subset = random_split(dataset, [train_size, val_size], torch.Generator().manual_seed(0))
 valid_dataset = UNetDataset(img_path, label_path, False)
-valid_dataset = Subset(valid_dataset, valid_subset.indices)
+valid_dataset = Subset(valid_dataset, valid_subset.indices) # Validation Dataset에는 elastic deformation 적용 X 위해
 
 train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
 val_loader = DataLoader(valid_dataset, batch_size=4, shuffle=False)
@@ -40,8 +41,19 @@ model = UNet().to(device)
 bceloss = BCELoss()
 
 # Optimizer #
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+optimizer = optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=1e-4)
+# optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 # optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.99)
+
+# Scheduler #
+scheduler = ReduceLROnPlateau(
+    optimizer,
+    mode='min', # loss 감소 기준 (max면 metric 증가 기준)
+    factor=0.5, # lr 감소 비율
+    threshold=1e-4, # 개선 기준
+    patience=2, # 개선 없으면 기다릴 epoch 수
+    min_lr=1e-6 # lr 최소값 제한
+)
 
 # Train #
 for epoch in range(1, num_epochs + 1):
@@ -63,9 +75,9 @@ for epoch in range(1, num_epochs + 1):
         train_loss += loss.item() * images.size(0)
 
         if (i + 1) % 10 == 0 or (i + 1) == len(train_loader):
-            print(f"Epoch [{epoch+1}/{num_epochs}], "
-                  f"Iter [{i+1}/{len(train_loader)}], "
-                  f"Loss: {train_loss/(i+1):.4f}")
+            print(f'Epoch [{epoch+1}/{num_epochs}], '
+                  f'Iter [{i+1}/{len(train_loader)}], '
+                  f'Loss: {train_loss/(i+1):.4f}')
 
     train_loss /= len(train_loader.dataset)
 
@@ -84,6 +96,8 @@ for epoch in range(1, num_epochs + 1):
             val_loss += loss.item() * images.size(0)
 
     val_loss /= len(val_loader.dataset)
+
+    scheduler.step(val_loss)
 
     print(f'Epoch [{epoch}/{epoch}] - Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}')
 
